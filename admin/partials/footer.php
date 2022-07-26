d5cc45989ecd45cadf62d23980d09c2f07b4d9fd
Sadia Islam <85259461+sadia799@users.noreply.github.com>
2022-07-27 00:04:33 +0600

    Update footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer Section Starts -->
 <div class="footer">
             <div class="wrapper">
-                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#"> Sadia &Aranya</a></p>
+                <p class="text-center">2022 All rights reserved, Food House. Developed By - <a href="#"> Sadia &Aranya</a></p>
             </div>
         </div>
         <!-- Footer Section Ends -->
